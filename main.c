caacee6bc572ae52b838d7c61e545a0609036add
Michael Vetter <jubalh@openmailbox.org>
2017-03-06 16:17:57 +0100

    Params for JID and password

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <strophe.h>
+#include <unistd.h> /* getopt */
 
 #define JID "test@jabber.org"
 #define PASSWORD "password"
@@ -90,7 +91,24 @@ int main(int argc, char *argv[])
    xmpp_conn_t *conn;
    xmpp_log_t *log;
 
+   char *pJid = JID;
+   char *pPassword = PASSWORD;
+
    printf("Hello World!\n");
+
+   int opt;
+   while ((opt = getopt(argc, argv, "j:p:")) != -1)
+   {
+      switch(opt)
+      {
+         case 'j':
+            pJid = strdup(optarg);
+            break;
+         case 'p':
+            pPassword = strdup(optarg);
+            break;
+      }
+   }
 
    xmpp_initialize();
 
@@ -102,8 +120,8 @@ int main(int argc, char *argv[])
 
    xmpp_conn_set_flags(conn, XMPP_CONN_FLAG_MANDATORY_TLS);
    xmpp_conn_set_keepalive(conn, 60, 1);
-   xmpp_conn_set_jid(conn, JID);
-   xmpp_conn_set_pass(conn, PASSWORD);
+   xmpp_conn_set_jid(conn, pJid);
+   xmpp_conn_set_pass(conn, pPassword);
 
    printf("connect\n");
    xmpp_connect_client(conn, "" , 0, conn_handler, ctx);
