d59e86b750de18bcd33daa77601b9b6a5c1e9ab4
Michael Vetter <jubalh@openmailbox.org>
2017-07-25 17:38:11 +0200

    Fix growing of buffer
    Let buffer grow so the whole output can be grasped.
    Thanks o.

--- xmpp.c
+++ xmpp.c
@@ -18,33 +18,44 @@ void parse(xmpp_ctx_t *ctx, char *message)
          message[3] == 'n' &&
          message[4] == ' ')
       {
-         FILE *outFile;
-         char buffer[512];
-         char *output;
-         size_t size;
+         FILE *out_file;
+         char *output_buffer;
+         size_t size_read;
+         size_t output_size = 0;
+         size_t off = 0;
 
-         outFile = popen(&message[5], "r");
-         if (!outFile)
+         out_file = popen(&message[5], "r");
+         if (!out_file)
          {
             fprintf(stderr, "Error whilte popen() from message: %s\n", message);
             return;
          }
 
-         output = malloc(ALLOC_SIZE);
-         output[0] = '\0';
-         do {
-            size = fread(buffer, sizeof(char), 512, outFile);
-            //buffer[size-1] = '\0';
-            if (size != 0 && strlen(buffer) + size + 1> sizeof(output))
-            {
-               output = realloc(output, sizeof(output) + ALLOC_SIZE + 1);
-            }
-            strncat(output, buffer, size);
-         }
-         while (size);
+         output_buffer = malloc(ALLOC_SIZE);
+         output_buffer[0] = '\0';
+         output_size = ALLOC_SIZE;
 
-         printf("output: %s\n", output);
-         fclose(outFile);
+         bool done = false;
+         while (!done) {
+             if (off >= output_size) {
+                 output_buffer = realloc(output_buffer, output_size*2);
+                 output_size *= 2;
+             }
+
+             size_t left = output_size - off;
+             size_t r = fread(output_buffer + off, 1, left, out_file);
+             if (r < left) {
+                 if (feof(out_file)) {
+                     done = true;
+                 } else {
+                     fprintf(stderr, "Error in fread\n");
+                 }
+             }
+             off += r;
+         }
+
+         printf("output: %s\n", output_buffer);
+         fclose(out_file);
       }
       else if (message[0] == '!' &&
          message[1] == 'q' &&
